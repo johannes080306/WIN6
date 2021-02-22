0cab52274b69b0d2c16fa472d61a7b7eca21bb4c
JRadd <JRadd@DESKTOP-UVA2D4A>
2021-02-22 13:54:15 +0100

    change

--- src/basics/Bubblesort.java
+++ src/basics/Bubblesort.java
@@ -1,25 +1,31 @@
 package basics;
 
+import java.util.Arrays;
+
 public class Bubblesort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] zahlen = {3, 2, 6, 9, 1, 2, 8, 5, 3, 1};
-		for (int a  = 1; a <= zahlen.length; a++) {
-		int left = zahlen[a];
-		int right= zahlen[a+1];
-		if(left > right) {
-		zahlen[a] = right;
-		zahlen[a + 1] = left;
-	
-		
+		int [] array = {3,2,6,9,1,2,8,5,3,1};
+		Arrays.sort(array);
+		for(int i = 0; i < array.length; i++) {
+        System.out.println(array[i]);
 		
 		
 		
 		}
 		}		
 		
+	public static int[] sort(int [] array) {
+	int temp = 0;
+	for (int i=0; i < array.length; i++) {
+	for (int j = 1; j < array.length; j++) {
+	
+	if(array[j]<array[j-1]) {
+	
 	}
-
+}
+	}
+}
 }
--- src/basics/zahlenungerade.java
+++ src/basics/zahlenungerade.java
@@ -4,8 +4,13 @@ public class zahlenungerade {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=2; i<101; i++)
+		for(int i=0; i<101; i++)
+			if(i%2 == 0) {
 			System.out.println(i);
 	}
-
+		for(int j=0; j<100; j++)
+		if(j%2 == 1)
+		System.out.println(j);
+		
+	}
 }
