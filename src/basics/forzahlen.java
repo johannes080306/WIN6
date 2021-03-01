910c78f54475a738cb3718db40086dc98d31bf6a
johannes.radda <johannes.radda@AMS-GYM-EDV1-04.gym.ams.local>
2021-03-01 13:57:28 +0100

    change

--- src/basics/forzahlen.java
+++ src/basics/forzahlen.java
@@ -4,7 +4,7 @@ public class forzahlen {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] zahlen = {3, 4, 2, 7, 5, 9, 8, 5};
+		int [] zahlen = {3, 2, 6, 9, 1, 2 ,8 ,5, 3, 1};
 		 for (int a  = 0; a < zahlen.length; a = a + 1) {
 
 	System.out.println(zahlen[a]);
