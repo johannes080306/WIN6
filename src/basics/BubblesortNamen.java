c0306948ac306210441106895ccaebece87fcab3
johannes.radda <johannes.radda@AMS-GYM-EDV1-04.gym.ams.local>
2021-03-01 15:19:25 +0100

    change

--- src/basics/BubblesortNamen.java
+++ src/basics/BubblesortNamen.java
@@ -5,41 +5,29 @@ public class BubblesortNamen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String liste[] = {"Max","Adrian","Tom","Felix","Niko"};
-	    for (int a = 0; a < liste.length; a++) {
-	    	System.out.println(liste[a]);}
-		
-	    	String temp;
-		
-		for (int j= 0; j < liste.length; j++) {
-			for (int i = j+1; i < liste.length; i++) {
-				if (liste[i].compareTo(liste[j]) <0)  {
-					temp = liste[j];
-					liste[j] = liste[i]                                   ;
-					liste[i] = temp;
-		                
-		                                        
-		
-		
-				}}
-		               
-		
-		 
-		System.out.println(liste[j ]);
-		
-		
-		
-		
-		
-		
-		
-				
-		
-		
-		
-	
+		String liste[] = { "Max", "Adrian", "Tom", "Felix", "Niko" };
+		System.out.println("Unsortier: ");
+		for (int a = 0; a < liste.length; a++) {
 
-
-}
+			System.out.println(liste[a]);
 		}
+
+		String temp;
+
+		for (int j = 0; j < liste.length; j++) {
+			for (int i = j + 1; i < liste.length; i++) {
+				if (liste[i].compareTo(liste[j]) < 0) {
+					temp = liste[j];
+					liste[j] = liste[i];
+					liste[i] = temp;
+				}
+			}
+		}
+		System.out.println("Unsortiert: ");
+		for (int a = 0; a < liste.length; a++) {
+
+			System.out.println(liste[a]);
+		}
+
 	}
+}
