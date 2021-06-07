277aea6ee3d1dce5db1c3bc85638ba21b0649dfa
johannes.radda <johannes.radda@AMS-GYM-EDV1-04.gym.ams.local>
2021-06-07 15:26:12 +0200

    change

--- src/basics/Rechner.java
+++ src/basics/Rechner.java
@@ -1,83 +1,64 @@
 package basics;
+
 import java.util.Scanner;
+
 public class Rechner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner eingabe = new Scanner(System.in);
-
+		int ersteZahl = 0;
 		while (true) {
 			if (eingabe.hasNext("[q]")) {
 				System.out.println("Der rechner ist aus");
 
 				break;
-				
-				
+
 			}
-				
-				
-				if (eingabe.hasNextInt()) {
-					int ersteZahl = eingabe.nextInt();
-
-					System.out.println("Zahl1: " + ersteZahl);
-
-					continue;
-				
-				
-					if (eingabe.hasNextInt()) {
-						int zweiteZahl = eingabe.nextInt();
-
-						System.out.println("Zahl2: " + zweiteZahl);
-						continue;
-						
-				
-						String operator = eingabe.next("[+-\\\\/*^]");
-					
-					
-
-					if (eingabe.hasNext("[+]")) {
-						
-						System.out.println(ersteZahl + zweiteZahl);
-						
-					} else if (eingabe.hasNext("[-]")) {
-						
-						System.out.println(ersteZahl - zweiteZahl);
-					} else if (eingabe.hasNext("[\\/]")){
-						
-						System.out.println(ersteZahl / zweiteZahl);
-					} else if (eingabe.hasNext("[*]")) {
-						
-						System.out.println(ersteZahl * zweiteZahl);
-					} else if (eingabe.hasNext("[^]")) {
-						
-						int ergebnis = (int) Math.pow(ersteZahl, zweiteZahl);
-						System.out.println(ergebnis);
-					} else if (eingabe.hasNext("[]]")) {
-						
-						System.out.println(Math.sqrt(ersteZahl));	
-					} 
-					
-					
-					
-					
-				
-
-					
-				
-					
-				
-
-	
-			
 
 			
-				
 
-		
+			if (eingabe.hasNextInt()) {
+				ersteZahl = eingabe.nextInt();
 
-					
-		
-		
-				
-					}}				}}}
+				System.out.println("Zahl1: " + ersteZahl);
 
-		
+				continue;
+
+			}
+
+			if (eingabe.hasNext("[]]")) {
+				String zeichen = eingabe.next("[]]");
+				System.out.println(Math.sqrt(ersteZahl));
+				continue;
+			}
+
+			if (eingabe.hasNext("[+]")) {
+				String zeichen = eingabe.next("[+]");
+				System.out.println(ersteZahl + eingabe.nextInt());
+				continue;
+			}
+			if (eingabe.hasNext("[-]")) {
+				String zeichen = eingabe.next("[-]");
+				System.out.println(ersteZahl - eingabe.nextInt());
+				continue;
+			}
+			if (eingabe.hasNext("[\\/]")) {
+				String zeichen = eingabe.next("[/]");
+				System.out.println(ersteZahl / eingabe.nextInt());
+				continue;
+			}
+			if (eingabe.hasNext("[+]")) {
+				String zeichen = eingabe.next("[+]");
+				System.out.println(ersteZahl + eingabe.nextInt());
+				continue;
+			}
+			if (eingabe.hasNext("[^]")) {
+				String zeichen = eingabe.next("[^]");
+				System.out.println(Math.pow(ersteZahl, eingabe.nextInt()));
+				continue;
+			}
+			eingabe.close();
+
+		}
+	}
+}
